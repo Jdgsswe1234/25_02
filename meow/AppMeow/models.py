ae92ac5f04177cc2e61b76f0cf9a37708c7f56f0
Jdgsswe1234 <zurovictimur@gmail.com>
2024-05-19 18:20:02 +0300

    h

--- meow/AppMeow/models.py
+++ meow/AppMeow/models.py
@@ -1,4 +1,4 @@
 from django.db import models
-class Clients(models.Model):
-    name = models.CharField("ФИО", max_length=50)
-    login = models.CharField("Логин", max_length=50)
+
+class Trash(models.Model):
+    name =
